6d3495c4b0bf680f5f1c5a05d658ac3bfd467e6c
YounnIm <younnus.iman@ucalgary.ca>
2025-03-15 19:30:17 -0600

    "Finished" Admin page, must update with database

--- movie-review-app/src/components/Admin.js
+++ movie-review-app/src/components/Admin.js
@@ -7,14 +7,32 @@ function Admin() {
   const [movieTitle, setMovieTitle] = useState('');
   const [movieDescription, setMovieDescription] = useState('');
   const [movieImage, setMovieImage] = useState('');
+  const [movies, setMovies] = useState([
+    { id: 1, title: 'Inception', description: 'A mind-bending thriller.', image: 'https://image.tmdb.org/t/p/w1280/ljsZTbVsrQSqZgWeep2B1QiDKuh.jpg' },
+    { id: 2, title: 'Mufasa: The Lion King', description: 'A journey into the past of the Lion King.', image: 'https://image.tmdb.org/t/p/w1280/9bXHaLlsFYpJUutg4E6WXAjaxDi.jpg' },
+    { id: 3, title: 'Captain America: Brave New World', description: 'Captain America returns in an epic adventure.', image: 'https://image.tmdb.org/t/p/w1280/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg' },
+  ]);
 
   const handleAddMovie = () => {
-    // TODO: Add backend logic to save movie data
-    console.log('Add Movie:', { movieTitle, movieDescription, movieImage });
-    // Clear form
-    setMovieTitle('');
-    setMovieDescription('');
-    setMovieImage('');
+    if (movieTitle && movieDescription && movieImage) {
+      const newMovie = {
+        id: movies.length + 1, // Simple ID generation (replace with a better method for production with backend database)
+        title: movieTitle,
+        description: movieDescription,
+        image: movieImage,
+      };
+      setMovies([...movies, newMovie]);
+      // Clear form
+      setMovieTitle('');
+      setMovieDescription('');
+      setMovieImage('');
+    } else {
+      alert('Please fill out all fields.');
+    }
+  };
+
+  const handleRemoveMovie = (id) => {
+    setMovies(movies.filter((movie) => movie.id !== id));
   };
 
   return (
@@ -44,6 +62,27 @@ function Admin() {
             Add Movie
           </button>
         </form>
+
+        <h2>Current Movies</h2>
+        <div className="movie-list">
+          {movies.map((movie) => (
+            <div key={movie.id} className="movie-item">
+              <img src={movie.image} alt={movie.title} className="movie-poster" />
+              <div className="movie-details">
+                <h3>{movie.title}</h3>
+                <p>{movie.description}</p>
+              </div>
+              <button
+                type="button"
+                onClick={() => handleRemoveMovie(movie.id)}
+                className="remove-button"
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+
         <button type="button" onClick={() => navigate('/')} className="guest-button">
           Back to Login
         </button>
